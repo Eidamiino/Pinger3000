52d7c6339e3a6bacacb644baf509371fda097691
Eidamiino <adam.pala199@gmail.com>
2022-04-27 21:02:13 +0200

    added timeout to ping

--- Pinger3000/Pinger3000/Program.cs
+++ Pinger3000/Pinger3000/Program.cs
@@ -17,29 +17,27 @@ namespace Pinger3000
 		public const int block = 25;
 		public const int yellowPing = 100;
 		public const int redPing = 200;
+		public const string blockChar = "█";
 		static void Main(string[] args)
 		{
 			Ping pingSender = new Ping();
-			// PingOptions options = new PingOptions();
-			//
-			// options.DontFragment = true;
-			//
-			// string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-			// byte[] buffer = Encoding.ASCII.GetBytes(data);
-			// int timeout = 120;
-			//nemám tušení co ten kod nahoře dělá, ale byl na microsoft dokumentaci a když ho odkomentuju do pingu tak to aji nekdy failuje
+			PingOptions options = new PingOptions();
+			options.DontFragment = true;
+			
+			string data = "hrochzirafamatrixjenejlepsiatom";
+			byte[] buffer = Encoding.ASCII.GetBytes(data);
+
+			int timeout = 120;
 			int counter = 0, sum = 0;
 			int success = 0, failed = 0;
-			string currentTime = DateTime.Now.ToString();
 			while(true)
 			{
 				{
-					PingReply reply = pingSender.Send(ipAdd);
+					PingReply reply = pingSender.Send(ipAdd,timeout,buffer,options);
 
 					if (reply.Status == IPStatus.Success)
 					{
-						var replyInfo = ReturnReplyInfo(reply);
-						pingResults.Add(replyInfo);
+						pingResults.Add(ReturnReplyInfo(reply));
 						
 						success++;
 						counter++;
@@ -47,9 +45,10 @@ namespace Pinger3000
 
 						RemoveFromListIfFull();
 						Console.Clear();
-						PrintReplyInfo(currentTime, reply, sum, counter, success, failed);
+						PrintReplyInfo(reply, sum, counter, success, failed);
 						Console.SetCursorPosition(0,startingRow);
-						PrintPingResult();
+						PrintList(pingResults);
+
 						Thread.Sleep(delay);
 					}
 					else
@@ -62,15 +61,16 @@ namespace Pinger3000
 
 		#region Printing
 
-		private static void PrintPingResult()
+		private static void PrintList(List<string> list)
 		{
-			for (int j = 0; j < pingResults.Count; j++)
+			for (int j = 0; j < list.Count; j++)
 			{
-				Console.WriteLine(pingResults[j]);
+				Console.WriteLine(list[j]);
 			}
 		}
-		private static void PrintReplyInfo(string currentTime, PingReply reply, int sum, int counter, int success, int failed)
+		private static void PrintReplyInfo(PingReply reply, int sum, int counter, int success, int failed)
 		{
+			string currentTime = DateTime.Now.ToString();
 			Console.Write($"Started: {currentTime}\t" +
 			              $"Successful: {success}\t" +
 			              $"Failed: {failed}\n" +
@@ -105,7 +105,7 @@ namespace Pinger3000
 		{
 			int i = 0;
 			do {
-				Console.Write("█");
+				Console.Write(blockChar);
 				i++;
 			} while (i < reply.RoundtripTime / block);
 		}
@@ -113,7 +113,7 @@ namespace Pinger3000
 		{
 			int i = 0;
 			do {
-				Console.Write("█");
+				Console.Write(blockChar);
 				i++;
 			} while (i < (sum/counter) / block);
 		}
@@ -148,9 +148,7 @@ namespace Pinger3000
 		}
 		private static string ReturnReplyInfo(PingReply reply)
 		{
-			string replyInfo =
-				$"Reply from {ipAdd}: bytes={reply.Buffer.Length} time={reply.RoundtripTime}ms TTL={reply.Options.Ttl}";
-			return replyInfo;
+			return $"Reply from {ipAdd}: bytes={reply.Buffer.Length} time={reply.RoundtripTime}ms TTL={reply.Options.Ttl}";
 		}
 
 	}
